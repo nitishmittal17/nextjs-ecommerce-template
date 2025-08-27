a43ff944ffc4599afd7a867f959cde6d4ccfbc21
Nitish Mittal <nitish.mittal@wingify.com>
2025-08-27 17:08:23 +0530

    updated account id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
       <head>
         {/* begin Convert Experiences code */}
         <Script 
-          src="//cdn-4.convertexperiments.com/v1/js/10017053-10017406.js?environment=production"
+          src="//cdn-4.convertexperiments.com/v1/js/10017288-10017622.js?environment=production"
           strategy="afterInteractive"
         />
         {/* end Convert Experiences code */}
