e2eb14938ccf2f2151f2cdfd1b5bde54ee90bc66
Apoorv Tiwari <apoorv.tiwari@wingify.com>
2026-01-07 20:45:49 +0530

    feat(VWOScript): commented unused code.

--- src/app/(site)/performance-test-vwo-sync/page.tsx
+++ src/app/(site)/performance-test-vwo-sync/page.tsx
@@ -1,15 +1,5 @@
 import Home from "@/components/Home";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "NextCommerce | Performance Test VWO Sync Page",
-  description: "Performance test page with VWO sync script",
-};
 
 export default function PerformanceTestVwoSyncPage() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <Home />;
 }
